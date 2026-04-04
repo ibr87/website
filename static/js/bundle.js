cdeb0105e4f906b8ef4c4f751c2a5f03c1bab1b1
ibr87 <avanakavni1985@gmail.com>
2026-04-04 17:41:48 +0300

    Add files via upload

--- static/js/bundle.js
+++ static/js/bundle.js
@@ -33142,56 +33142,176 @@ function App() {
         fileName: _jsxFileName,
         lineNumber: 58,
         columnNumber: 9
-      }, this), /*#__PURE__*/(0,react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_13__.jsxDEV)("a", {
-        href: "https://wa.me/905051736910",
-        target: "_blank",
-        rel: "noopener noreferrer",
-        className: "whatsapp-btn",
+      }, this), /*#__PURE__*/(0,react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_13__.jsxDEV)("div", {
         style: {
           position: "fixed",
-          bottom: "80px",
-          right: isRtl ? "auto" : "20px",
-          left: isRtl ? "20px" : "auto",
-          zIndex: 9998
+          bottom: "20px",
+          right: "20px",
+          zIndex: 9998,
+          display: "flex",
+          flexDirection: "column",
+          gap: 10
         },
-        "data-testid": "whatsapp-btn",
-        title: "WhatsApp ile \u0130leti\u015Fim",
         "x-file-name": "App",
         "x-line-number": "60",
         "x-column": "8",
-        "x-component": "a",
+        "x-component": "div",
         "x-id": "App_60_8",
         "x-dynamic": "false",
-        children: /*#__PURE__*/(0,react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_13__.jsxDEV)("svg", {
-          width: "28",
-          height: "28",
-          viewBox: "0 0 24 24",
-          fill: "white",
+        children: [/*#__PURE__*/(0,react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_13__.jsxDEV)("a", {
+          href: "https://www.instagram.com/dr_ibrahim_oguz/",
+          target: "_blank",
+          rel: "noopener noreferrer",
+          style: {
+            width: 52,
+            height: 52,
+            borderRadius: "50%",
+            background: "linear-gradient(135deg, #f09433 0%, #e6683c 25%, #dc2743 50%, #cc2366 75%, #bc1888 100%)",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            boxShadow: "0 4px 16px rgba(220,39,67,0.4)",
+            transition: "transform 0.2s"
+          },
+          onMouseEnter: e => e.currentTarget.style.transform = "scale(1.1)",
+          onMouseLeave: e => e.currentTarget.style.transform = "scale(1)",
+          title: "Instagram",
+          "data-testid": "float-instagram-btn",
           "x-file-name": "App",
-          "x-line-number": "69",
+          "x-line-number": "62",
           "x-column": "10",
-          "x-component": "svg",
-          "x-id": "App_69_10",
+          "x-component": "a",
+          "x-id": "App_62_10",
           "x-dynamic": "false",
-          children: /*#__PURE__*/(0,react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_13__.jsxDEV)("path", {
-            d: "M17.472 14.382c-.297-.149-1.758-.867-2.03-.967-.273-.099-.471-.148-.67.15-.197.297-.767.966-.94 1.164-.173.199-.347.223-.644.075-.297-.15-1.255-.463-2.39-1.475-.883-.788-1.48-1.761-1.653-2.059-.173-.297-.018-.458.13-.606.134-.133.298-.347.446-.52.149-.174.198-.298.298-.497.099-.198.05-.371-.025-.52-.075-.149-.669-1.612-.916-2.207-.242-.579-.487-.5-.669-.51-.173-.008-.371-.01-.57-.01-.198 0-.52.074-.792.372-.272.297-1.04 1.016-1.04 2.479 0 1.462 1.065 2.875 1.213 3.074.149.198 2.096 3.2 5.077 4.487.709.306 1.262.489 1.694.625.712.227 1.36.195 1.871.118.571-.085 1.758-.719 2.006-1.413.248-.694.248-1.289.173-1.413-.074-.124-.272-.198-.57-.347m-5.421 7.403h-.004a9.87 9.87 0 01-5.031-1.378l-.361-.214-3.741.982.998-3.648-.235-.374a9.86 9.86 0 01-1.51-5.26c.001-5.45 4.436-9.884 9.888-9.884 2.64 0 5.122 1.03 6.988 2.898a9.825 9.825 0 012.893 6.994c-.003 5.45-4.437 9.884-9.885 9.884m8.413-18.297A11.815 11.815 0 0012.05 0C5.495 0 .16 5.335.157 11.892c0 2.096.547 4.142 1.588 5.945L.057 24l6.305-1.654a11.882 11.882 0 005.683 1.448h.005c6.554 0 11.89-5.335 11.893-11.893a11.821 11.821 0 00-3.48-8.413z",
+          children: /*#__PURE__*/(0,react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_13__.jsxDEV)("svg", {
+            width: "26",
+            height: "26",
+            viewBox: "0 0 24 24",
+            fill: "none",
+            stroke: "white",
+            strokeWidth: "2",
+            strokeLinecap: "round",
+            strokeLinejoin: "round",
             "x-file-name": "App",
-            "x-line-number": "70",
+            "x-line-number": "78",
             "x-column": "12",
-            "x-component": "path",
-            "x-id": "App_70_12",
-            "x-dynamic": "false"
-          }, void 0, false, {
+            "x-component": "svg",
+            "x-id": "App_78_12",
+            "x-dynamic": "false",
+            children: [/*#__PURE__*/(0,react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_13__.jsxDEV)("rect", {
+              x: "2",
+              y: "2",
+              width: "20",
+              height: "20",
+              rx: "5",
+              ry: "5",
+              "x-file-name": "App",
+              "x-line-number": "79",
+              "x-column": "14",
+              "x-component": "rect",
+              "x-id": "App_79_14",
+              "x-dynamic": "false"
+            }, void 0, false, {
+              fileName: _jsxFileName,
+              lineNumber: 79,
+              columnNumber: 15
+            }, this), /*#__PURE__*/(0,react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_13__.jsxDEV)("path", {
+              d: "M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z",
+              "x-file-name": "App",
+              "x-line-number": "79",
+              "x-column": "70",
+              "x-component": "path",
+              "x-id": "App_79_70",
+              "x-dynamic": "false"
+            }, void 0, false, {
+              fileName: _jsxFileName,
+              lineNumber: 79,
+              columnNumber: 71
+            }, this), /*#__PURE__*/(0,react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_13__.jsxDEV)("line", {
+              x1: "17.5",
+              y1: "6.5",
+              x2: "17.51",
+              y2: "6.5",
+              "x-file-name": "App",
+              "x-line-number": "79",
+              "x-column": "129",
+              "x-component": "line",
+              "x-id": "App_79_129",
+              "x-dynamic": "false"
+            }, void 0, false, {
+              fileName: _jsxFileName,
+              lineNumber: 79,
+              columnNumber: 130
+            }, this)]
+          }, void 0, true, {
             fileName: _jsxFileName,
-            lineNumber: 70,
+            lineNumber: 78,
             columnNumber: 13
           }, this)
         }, void 0, false, {
           fileName: _jsxFileName,
-          lineNumber: 69,
+          lineNumber: 62,
           columnNumber: 11
-        }, this)
-      }, void 0, false, {
+        }, this), /*#__PURE__*/(0,react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_13__.jsxDEV)("a", {
+          href: "https://wa.me/905051736910",
+          target: "_blank",
+          rel: "noopener noreferrer",
+          style: {
+            width: 52,
+            height: 52,
+            borderRadius: "50%",
+            background: "#25D366",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            boxShadow: "0 4px 16px rgba(37,211,102,0.4)",
+            transition: "transform 0.2s"
+          },
+          onMouseEnter: e => e.currentTarget.style.transform = "scale(1.1)",
+          onMouseLeave: e => e.currentTarget.style.transform = "scale(1)",
+          title: "WhatsApp",
+          "data-testid": "whatsapp-btn",
+          "x-file-name": "App",
+          "x-line-number": "83",
+          "x-column": "10",
+          "x-component": "a",
+          "x-id": "App_83_10",
+          "x-dynamic": "false",
+          children: /*#__PURE__*/(0,react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_13__.jsxDEV)("svg", {
+            width: "28",
+            height: "28",
+            viewBox: "0 0 24 24",
+            fill: "white",
+            "x-file-name": "App",
+            "x-line-number": "99",
+            "x-column": "12",
+            "x-component": "svg",
+            "x-id": "App_99_12",
+            "x-dynamic": "false",
+            children: /*#__PURE__*/(0,react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_13__.jsxDEV)("path", {
+              d: "M17.472 14.382c-.297-.149-1.758-.867-2.03-.967-.273-.099-.471-.148-.67.15-.197.297-.767.966-.94 1.164-.173.199-.347.223-.644.075-.297-.15-1.255-.463-2.39-1.475-.883-.788-1.48-1.761-1.653-2.059-.173-.297-.018-.458.13-.606.134-.133.298-.347.446-.52.149-.174.198-.298.298-.497.099-.198.05-.371-.025-.52-.075-.149-.669-1.612-.916-2.207-.242-.579-.487-.5-.669-.51-.173-.008-.371-.01-.57-.01-.198 0-.52.074-.792.372-.272.297-1.04 1.016-1.04 2.479 0 1.462 1.065 2.875 1.213 3.074.149.198 2.096 3.2 5.077 4.487.709.306 1.262.489 1.694.625.712.227 1.36.195 1.871.118.571-.085 1.758-.719 2.006-1.413.248-.694.248-1.289.173-1.413-.074-.124-.272-.198-.57-.347m-5.421 7.403h-.004a9.87 9.87 0 01-5.031-1.378l-.361-.214-3.741.982.998-3.648-.235-.374a9.86 9.86 0 01-1.51-5.26c.001-5.45 4.436-9.884 9.888-9.884 2.64 0 5.122 1.03 6.988 2.898a9.825 9.825 0 012.893 6.994c-.003 5.45-4.437 9.884-9.885 9.884m8.413-18.297A11.815 11.815 0 0012.05 0C5.495 0 .16 5.335.157 11.892c0 2.096.547 4.142 1.588 5.945L.057 24l6.305-1.654a11.882 11.882 0 005.683 1.448h.005c6.554 0 11.89-5.335 11.893-11.893a11.821 11.821 0 00-3.48-8.413z",
+              "x-file-name": "App",
+              "x-line-number": "100",
+              "x-column": "14",
+              "x-component": "path",
+              "x-id": "App_100_14",
+              "x-dynamic": "false"
+            }, void 0, false, {
+              fileName: _jsxFileName,
+              lineNumber: 100,
+              columnNumber: 15
+            }, this)
+          }, void 0, false, {
+            fileName: _jsxFileName,
+            lineNumber: 99,
+            columnNumber: 13
+          }, this)
+        }, void 0, false, {
+          fileName: _jsxFileName,
+          lineNumber: 83,
+          columnNumber: 11
+        }, this)]
+      }, void 0, true, {
         fileName: _jsxFileName,
         lineNumber: 60,
         columnNumber: 9
@@ -41791,7 +41911,7 @@ if (typeof Promise !== 'undefined' && $ReactRefreshCurrentExports$ instanceof Pr
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("0c83881672626c350aeb")
+/******/ 		__webpack_require__.h = () => ("9f635eda54aa674d15f2")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
